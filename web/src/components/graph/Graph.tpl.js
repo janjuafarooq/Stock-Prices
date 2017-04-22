92733f9be159021da24b7d075eaacba9fac5de38
Farooq <janjua.farooq@10-123-223-224.vlan906.client.wireless.uic.edu>
2017-04-22 14:11:05 -0500

    Code cleanup

--- web/src/components/graph/Graph.tpl.js
+++ web/src/components/graph/Graph.tpl.js
@@ -2,28 +2,24 @@ import React from 'react';
 import { CandlestickChart } from 'react-d3';
 
 export function GraphTemplate(component) {
-    const data = {
-        values: []
-    };
-
-    data.values = component.state.historicalData.map(item => {
-        return { x: new Date(item.date), open: item.open, high: item.high, low: item.low, close: item.close };
-    });
-
-    let ohlcData = [];
-
-    if (data.values.length > 0) {
-        ohlcData = [
-            {
-                values: data.values
-            }
-        ];
-    }
+    const data = [
+        {
+            values: component.state.historicalData.map(item => {
+                return {
+                    x: new Date(item.date),
+                    open: item.open,
+                    high: item.high,
+                    low: item.low,
+                    close: item.close
+                };
+            })
+        }
+    ];
 
     return (
         <div className="row">
             <CandlestickChart
-                data={ohlcData}
+                data={data}
                 width={500}
                 height={400}
                 xAxisTickInterval={{ unit: 'month', interval: 1 }}
