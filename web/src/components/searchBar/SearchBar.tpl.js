68736ff6ab32d31608e43b424950f70133d6f370
Farooq <janjua.farooq@10-136-219-167.vlan908.client.wireless.uic.edu>
2017-04-23 10:56:43 -0500

    Fixing center issue

--- web/src/components/searchBar/SearchBar.tpl.js
+++ web/src/components/searchBar/SearchBar.tpl.js
@@ -4,7 +4,7 @@ export function SearchBarTemplate(component) {
     return (
         <div className="col-sm-8" style={{ display: 'inline-block', float: 'none', textAlign: 'left', marginRight: '-4px', marginBottom: '30px' }}>
             <h2 style={{ textAlign: 'center' }}>Search for companies</h2>
-            <div className="input-group col-md-12" style={{ margin: '0px 40px' }}>
+            <div className="input-group col-md-12">
                 <input type="text" name="searchText" className="form-control input-lg" placeholder="Enter company name or symbol" onChange={component.handleSearchTextChange} onKeyPress={component.handleSearchTextChange} />
                 <div className="input-group-btn">
                     <button className="btn btn-info btn-lg" type="button" value="Search" onClick={component.handleSearch}>
