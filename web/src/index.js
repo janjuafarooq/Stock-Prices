c2caa764f14803309762725ace671c6d332daeb6
Farooq <janjua.farooq@10-123-223-224.vlan906.client.wireless.uic.edu>
2017-04-30 00:33:13 -0500

    Use minified css.

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/main/Main.component.js';
-import 'bootstrap/dist/css/bootstrap.css';
 import config from './config.js'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Main {...config} />,
