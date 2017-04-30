67da4209d0aa5e32e29458bb200929067156b2bd
Farooq <janjua.farooq@10-123-223-224.vlan906.client.wireless.uic.edu>
2017-04-30 11:11:01 -0500

    Rejecting other routes.

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,7 +6,11 @@ router.use('/companyData', require('./companyData'));
 router.use('/companylist', require('./companyList'));
 
 router.get('/', function (req, res, next) {
-    res.status(403).send('Not allowed');
+    res.status(403).send();
+});
+
+router.get('*', function (req, res, next) {
+    res.status(404).send();
 });
 
 module.exports = router;
